8e5d32561f83cea5e4d272d7ce0e41f68c5fa391
SeungyeonLee <angela7199@naver.com>
2020-01-07 21:04:14 +0900

    Changed

--- Seungyeon/src/sy/pr_2_탑.java
+++ Seungyeon/src/sy/pr_2_탑.java
@@ -2,38 +2,47 @@ package sy;
 
 import java.util.*;
 
-public class pr_2_ž {
+public class pr_2_탑 {
 
 	public static void solution(int[] heights) {
 
 		ArrayList<Integer> list = new ArrayList<Integer>();
+		//boolean chk = false;
 
-		for (int i = heights.length-1; i >= 0; i--) {
-			for (int j = i-1; j >= 0; j--) {
-			
+		for (int i = heights.length - 1; i > 0; i--) {
+			boolean chk = false;
+			for (int j = i - 1; j >= 0; j--) {
+
 				if (heights[i] < heights[j]) {
 					list.add(j + 1);
-					
+					chk = true;
 					break;
 				}
 
-				else{
-					list.add(0);
-				}
 			}
+			
+			if (chk == false) {
+				list.add(0);
+			}
+			
+//			chk = false;
+			
+
 		}
-		
-		for(int i=0;i<list.size();i++) {
+		list.add(0);
+
+		Collections.reverse(list);
+
+		for (
+
+				int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i));
 		}
 
 	}
-	
 
 	public static void main(String[] args) {
 		int[] arr = { 6, 9, 5, 7, 4 };
 		solution(arr);
 	}
 }
-
-
