3783049407a070b5c155becf23ce68fbc0c8e1bc
SeungyeonLee <angela7199@naver.com>
2020-01-08 16:14:49 +0900

    algo

--- Seungyeon/src/sy/pr_2_탑.java
+++ Seungyeon/src/sy/pr_2_탑.java
@@ -4,19 +4,18 @@ import java.util.*;
 
 public class pr_2_탑 {
 
-	public static void solution(int[] heights) {
+	public static int[] solution(int[] heights) {
 
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		//boolean chk = false;
-
+	
 		for (int i = heights.length - 1; i > 0; i--) {
-			boolean chk = false;
-			for (int j = i - 1; j >= 0; j--) {
+			boolean chk = false;// 판별해주기 위한 false
+			for (int j = i - 1; j >= 0; j--) {//배열의 마지막 값에서 하나 적은 칸의 값 
 
 				if (heights[i] < heights[j]) {
 					list.add(j + 1);
 					chk = true;
-					break;
+					break;//list에 add해 준 후에는 break
 				}
 
 			}
@@ -24,20 +23,18 @@ public class pr_2_탑 {
 			if (chk == false) {
 				list.add(0);
 			}
-			
-//			chk = false;
-			
 
 		}
-		list.add(0);
+		list.add(0);// 1층은 더이상 내려갈 수 없으니까 0을 출력
 
 		Collections.reverse(list);
 
-		for (
-
-				int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
+		int answer[]= new int[list.size()];
+		for(int i=0;i<list.size();i++) {
+			answer[i]+=list.get(i);
+			
 		}
+		return answer;
 
 	}
 
@@ -45,4 +42,5 @@ public class pr_2_탑 {
 		int[] arr = { 6, 9, 5, 7, 4 };
 		solution(arr);
 	}
+
 }
