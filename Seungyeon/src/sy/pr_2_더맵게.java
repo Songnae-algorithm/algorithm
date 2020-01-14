db2cf4e9c5596fd823e36cc0fb0f987bc46066af
SeungyeonLee <angela7199@naver.com>
2020-01-15 00:23:01 +0900

    다음큰숫자

--- Seungyeon/src/sy/pr_2_더맵게.java
+++ Seungyeon/src/sy/pr_2_더맵게.java
@@ -6,33 +6,31 @@ public class pr_2_더맵게 {
 	public static int solution(int[] scoville, int K) {
 		int answer = 0;
 
-		PriorityQueue<Integer> list = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> list = new PriorityQueue<Integer>();//우선순위 큐 선언
 
 		for (int i = 0; i < scoville.length; i++) {
-			list.add(scoville[i]);
+			list.add(scoville[i]);//큐에 배열 값 입력 
 		}
 
-		while (list.peek() < K) {
+		while (list.peek() < K) {//peek : 최상위 값만 보여주는것!!
 
-			int a = list.poll();
-			// System.out.println(a);
+			int a = list.poll();//poll : 최상위값을 제거 (remove도 사용 O) 
+
 			int b = list.poll();
-			// System.out.println(b);
-			int c = a + (b * 2);
-			// System.out.println(c);
+
+			int c = a + (b * 2); 
+
 			list.add(c);
-			// System.out.println(list);
 
 			answer++;
 
-			// System.out.println(list);
-			if (list.size() == 1) {
+			if (list.size() == 1 && list.peek() < K) {
+				//스코빌을 계속 하고 입력한 마지막 값이 1개이고,
+				//그게 K보다 작으면 안대니까 -1을 리턴해준다
 				return -1;
 			}
 
 		}
-
-		// System.out.println(answer);
 		return answer;
 
 	}
