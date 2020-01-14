e555656dbd3d24a790ca93742470b3e6946b9e51
SeungyeonLee <angela7199@naver.com>
2020-01-14 19:37:17 +0900

    Change

--- Seungyeon/src/sy/pr_2_전화번호목록.java
+++ Seungyeon/src/sy/pr_2_전화번호목록.java
@@ -3,19 +3,20 @@ package sy;
 import java.util.*;
 
 public class pr_2_전화번호목록 {
+	//성공코드
 
 	public static boolean solution(String[] phone_book) {
 		boolean answer = true;
 
 		Arrays.sort(phone_book);
+		
+		//정렬하면 앞에있는 문자열들이 제일 작기 때문에 정렬해주면 한번만 돌아도 된당
 		for (int i = 0; i < phone_book.length; i++) {
 			for (int j = i+1; j < phone_book.length; j++) {
-
-				if (i == j) {
-					continue;
-				}
-
+				
 				if (phone_book[j].startsWith(phone_book[i])) {
+					//startsWith를 써서 접두어 체크 
+
 					answer = false;
 					break;
 				}
