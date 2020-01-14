cd5f0d5d140fe8638bedab4851ea4e34ef460246
Parksuu <pigparksubin@naver.com>
2020-01-14 19:16:15 +0900

    전화번호목록

---
+++ subin/src/Greedy/prog_2_조이스틱.java
@@ -0,0 +1,30 @@
+package Greedy;
+
+import java.util.*;
+
+public class prog_2_¡∂¿ÃΩ∫∆Ω {
+
+    public int solution(String name) {
+        int answer = 0;
+        int arr[] = new int[name.length()];
+        
+        
+        for(int i=0; i<name.length(); i++)
+        	arr[i] = Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);
+        
+        
+        while(true) {
+        	
+        	
+        	
+        }
+        
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+	}
+}
+ 
---
+++ subin/src/Hash/prog_2_전화번호목록.java
@@ -0,0 +1,36 @@
+package Hash;
+
+import java.util.*;
+
+public class prog_2_전화번호목록 {
+	
+    static public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+    	HashMap<String,String> map = new HashMap<String,String>();
+    	
+    	for(int i=0;i<phone_book.length;i++)
+    		map.put(phone_book[i], phone_book[i]);
+    	
+    	for(int i=0;i<map.size();i++){
+    		String a = map.get(phone_book[i]);
+    		for(int j=i+1; j<map.size(); j++) {
+    			if(a.startsWith(phone_book[j]) || phone_book[j].startsWith(a)) {
+    				answer = false;
+    				break;
+    			}
+    		}
+    		
+    		if(answer == false)
+    			break;
+    	}
+
+        return answer;
+    }
+    
+    
+	public static void main(String[] args) {
+		
+	}
+	
+}
