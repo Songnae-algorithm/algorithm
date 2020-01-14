d44df345c78c4458739508e0e0dfdec9aa33e404
hengmo <cjsoung@naver.com>
2020-01-14 21:15:19 +0900

    더 맵게 update

--- 준성/더맵게 -프로그래머스.py
+++ 준성/더맵게 -프로그래머스.py
@@ -7,25 +7,12 @@ def solution(scoville, K):
     check = False
     length = len(scoville)
     
-    while i < length:
-        if scoville[0] < K:
-            mixScoville = scoville[0] + scoville[1] * 2
-            heapq.heappop(scoville)
-            heapq.heappop(scoville)
-            count += 1
-            heapq.heappush(scoville, mixScoville)
-            i = 0
-            length = len(scoville)
-            check = True
-        else:
-            i += 1
-            
-    if count != 0:
-        return count
-    elif check == True and count == 0:
-        return 0
-    elif check == False:
-        return -1
+    while scoville[0] < K:
+        mixScoville = scoville[0] + scoville[1] * 2
+        heapq.heappop(scoville)
+        heapq.heappop(scoville)
+        count += 1
+        heapq.heappush(scoville, mixScoville)
         
                 
 
