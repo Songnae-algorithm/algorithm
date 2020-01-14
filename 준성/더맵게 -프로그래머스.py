a1a36ec68d133a4c59771ded1f338a70b7490622
hengmo <cjsoung@naver.com>
2020-01-14 21:39:51 +0900

    더 맵게 update

--- 준성/더맵게 -프로그래머스.py
+++ 준성/더맵게 -프로그래머스.py
@@ -2,18 +2,17 @@ import heapq
 
 def solution(scoville, K):
     heapq.heapify(scoville)
-    i = 0
     count = 0
-    check = False
-    length = len(scoville)
     
-    while scoville[0] < K:
+    while scoville[0] <= K:
         mixScoville = scoville[0] + scoville[1] * 2
         heapq.heappop(scoville)
         heapq.heappop(scoville)
-        count += 1
         heapq.heappush(scoville, mixScoville)
+        count += 1
         
-                
-
-    
+        if scoville[0] < K and len(scoville) == 1:
+            count = -1
+            break
+        
+    return count
