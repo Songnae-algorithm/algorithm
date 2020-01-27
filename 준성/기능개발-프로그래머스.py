90615b4a0073475cb9450aa87908faddb8037b4a
hengmo <cjsoung@naver.com>
2020-01-27 12:28:14 +0900

    기능개발, 네트워크

---
+++ 준성/기능개발-프로그래머스.py
@@ -0,0 +1,29 @@
+def solution(progresses, speeds):
+  # 완성(while문 수정하고 싶다)
+    queue = []
+    count = 0
+    answer = []
+    for i in range(len(progresses)):
+        rest = (100 - progresses[i]) % speeds[i]
+        temp = (100 - progresses[i]) // speeds[i]
+        if rest == 0:
+            queue.append(temp)
+        else:
+            queue.append(temp + 1)
+    
+    
+    day = queue.pop(0)
+    count += 1
+    
+    while queue:
+        if day >= queue[0]:
+            count += 1
+            queue.pop(0)
+        else:
+            answer.append(count)
+            day = queue.pop(0)
+            count = 1
+            
+    answer.append(count)
+    return answer
+            
---
+++ 준성/네트워크-프로그래머스.py
@@ -0,0 +1,24 @@
+def dfs(v, visited, adj):
+    visited[v] = True
+    for e in adj[v]:
+        if not visited[e]:
+            dfs(e, visited, adj)
+
+def solution(n, computers):
+  # 완성 코드(다시 풀어보기)
+    count = 0
+    adj = [[] for _ in range(n)]
+    for i in range(len(computers)):
+        for k in range(len(computers[i])):
+            if i == k:
+                continue
+            elif computers[i][k] == 1:
+                adj[i].append(k)
+                
+    visited = [False] * (n)
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, visited, adj)
+            count += 1
+            
+    return count
