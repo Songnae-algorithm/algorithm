6f0ae2345426eddabcae02b66a9895b763aeaea9
hengmo <cjsoung@naver.com>
2020-02-20 15:25:46 +0900

    다리를 지나는 트럭

---
+++ 준성/다리를 지나는 트럭-프로그래머스.py
@@ -0,0 +1,15 @@
+def solution(bridge_length, weight, truck_weights):
+    # 완료(다시 1회)
+    time = 0
+    q = [0] * bridge_length
+    
+    while q:
+        time += 1
+        q.pop(0)
+        if truck_weights:
+            if sum(q) + truck_weights[0] <= weight:
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
+    
+    return time
