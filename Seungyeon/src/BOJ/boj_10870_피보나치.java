ead62cd6643d1dc4f612433d84f3ca3529c6c48b
SeungyeonLee <angela7199@naver.com>
2020-03-02 18:38:04 +0900

    피보나치

---
+++ Seungyeon/src/BOJ/boj_10870_피보나치.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.util.*;
+
+public class boj_10870_피보나치 {
+	static int ans = 0;
+
+	public static int fib(int a) {
+
+		if (a <= 1) {
+			ans = a;//1보다 작거나 같을때 a 리턴 (f(0)=0, f(1) = 1 리턴 ) 
+		}
+
+		else {
+			ans = fib(a - 1) + fib(a - 2); //2보다 클 때
+		}
+
+		return ans;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+
+		fib(a);
+		System.out.println(ans);
+
+	}
+
+}
