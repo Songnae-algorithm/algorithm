7d00b762b66526726726153b3af780ade2afbd6d
Parksuu <pigparksubin@naver.com>
2020-03-11 21:50:47 +0900

    1 s

---
+++ subin/src/Greedy/beak_문자열.java
@@ -0,0 +1,58 @@
+package Greedy;
+
+import java.util.*;
+
+public class beak_문자열 {
+
+    static public void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.nextLine();
+        String s[] = a.split(" "); //s[0]이 s[1]보다 작다
+             
+        int count = 0;            	int min = 100;
+        if(s[0].length() == s[1].length()){
+            for(int i=0; i<s.length; i++) {
+            	if(s[0].charAt(i) != s[1].charAt(i))
+            		count++;
+            }
+        }
+        else{
+            for(int i=0; i<s[1].length()-s[0].length()+1; i++) {
+            	count = 0;
+            	String ss = s[0]; int front = i-1; int back = i+ s[0].length();
+            	
+            	while(ss.length() != s[1].length()) {
+            		if(front<0 && back <s[1].length()) {
+            			ss =  ss + "*";
+            			back++;
+            		}
+            		else if(front>=0 && back >=s[1].length()) {
+            			ss= "*" + ss;
+            			front--;
+            		}
+            		else if(front>=0 && back <s[1].length()){
+            			ss = "*" + ss + "*";
+            			front--; back++;
+            		}
+            	}//while
+            	
+            	System.out.println(ss);
+            	
+            	for(int j=0; j<s[1].length(); j++) {
+                	if(ss.charAt(j) != s[1].charAt(j) || ss.charAt(j) == '*') {
+                		count++;
+                		
+                	}
+                		
+                }
+            	
+            	min = Math.min(count, min);
+            }//for
+            
+            count =min;
+        }
+        
+        System.out.println(count);
+    }
+}
