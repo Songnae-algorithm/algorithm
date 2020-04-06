8c70feebdc73287aa4fc8b4e2042b61945c02704
Seungyeon <angela7199@naver.com>
2020-04-06 16:30:22 +0900

    여행경로

--- syeon/src/sort/pr3_여행경로2.java
+++ syeon/src/sort/pr3_여행경로2.java
@@ -4,62 +4,58 @@ import java.util.*;
 
 public class pr3_여행경로2 {
 
-	public static String[] solution(String[][] tickets) {
-		String[] answer = {};
+	static boolean chk[];
+	static ArrayList<String> list = new ArrayList<>();
+	static String str = "";
 
-		Arrays.sort(tickets, new Comparator<String[]>() {
+	private static void dfs(String[][] arr, boolean chk[], String str, String end, int cnt) {
+		str += end + ",";
+		
+		if (cnt == arr.length) {// cnt랑 길이가 똑같아지면
+			list.add(str);
+			return;
+		}
+// ICN으로 시작해서 길이가 같은 모든 경로를 찾아서 return 해 줌 
+		for (int i = 0; i < arr.length; i++) {
+			if (chk[i] == false && arr[i][0].equals(end)) { // INC-JFK 라고 쳤을 때 end는 JFK. JFK로
+															// 시작하는 (0번째 배열)
+				chk[i] = true;// 갔으니까 true
 
-			public int compare(String a[], String b[]) {
-				if (a[1].charAt(0) == b[1].charAt(0)) {
-					return a[0].charAt(0) - b[0].charAt(0);
-				} else {
-					return a[1].charAt(0) - b[1].charAt(0);
-				}
-			}
-		});
+				dfs(arr, chk, str, arr[i][1], cnt + 1);// 한번 갈때마다 cnt ++ 해주고, arr 길이랑 같을때까지만 함
 
-		boolean chk[] = new boolean[tickets.length];
-		Queue<String> qu = new LinkedList<>();
-		ArrayList<String> list = new ArrayList<>();
-		int cnt = 1;
-		list.add("ICN");
-
-		for (int i = 0; i < tickets.length; i++) {
-
-			if (tickets[i][0].equals("ICN")) {
-				qu.add(tickets[i][1]);
-				chk[i] = true;
-				list.add(tickets[i][1]);
-
-				while (!qu.isEmpty()) {
-
-					String str = qu.poll();
-
-					for (int j = 0; j < tickets.length; j++) {
-
-						if (tickets[j][0].equals(str) && chk[j] == false) {
-							qu.add(tickets[j][1]);
-
-							list.add(tickets[i][1]);
-							cnt++;
-							chk[j] = true;
-							System.out.println(tickets[i][0] + "     " + tickets[i][1]);
-						}
-					}
-				}
-
+				chk[i] = false;
+				//여기서 왜 false로 돌려주는지 궁금 ~~~
 			}
 		}
-		System.out.println(list);
-		return answer;
+	}
 
+	public static String[] solution(String[][] tickets) {
+
+		for (int i = 0; i < tickets.length; i++) {
+			if (tickets[i][0].equals("ICN")) {
+				chk = new boolean[tickets.length];// 갔는지 체크
+				chk[i] = true; // ICN있는곳을 도니까
+				//
+				dfs(tickets, chk, "ICN,", tickets[i][1], 1);
+				//chk[i] = false;
+			}
+		}
+
+		Collections.sort(list); 
+		//ICN으로 시작해서 길이가 같은 모든 경로를 가지고있음. 문자열 순서로 정렬하기로 했으니까 정렬해준다 
+		
+		String[] answer = list.get(0).split(",");
+		//정렬이 되었으니까 첫번째 list배열에 있는 값들을 ","로 잘라서 넣는다 
+
+		
+		return answer;
 	}
 
 	public static void main(String[] args) {
 		String arr[][] = { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } };
 
 		String arr2[][] = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" }, { "ATL", "SFO" } };
-		solution(arr2);
-	}
 
+		solution(arr);
+	}
 }
