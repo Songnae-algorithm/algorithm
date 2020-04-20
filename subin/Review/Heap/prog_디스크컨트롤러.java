64a133a6fcf697fe57b98ee7998ee87fc71b6d58
Parksuu <pigparksubin@naver.com>
2020-04-20 23:28:08 +0900

    re, 디스크컨트롤러

--- subin/Review/Heap/prog_디스크컨트롤러.java
+++ subin/Review/Heap/prog_디스크컨트롤러.java
@@ -16,56 +16,51 @@ public class prog_디스크컨트롤러 {
 		@Override
 		public int compareTo(Disk d) {
 			//일하는 시간이 같으면 빨리들어오는애를! 오름차순으로
-			if(this.job == d.job) {
-				if(this.sec > d.sec)
-					return 1;
-				else
-					 return -1;
-			}
-			//지금 들어온 애가 일하는시간이 더 크면! 반대로! 내림차순!
-			else if(this.job < d.job)
-				return -1;
-			//지금 들어온 애가 작으면 오름차순!
-			else
-				return 1;
+			if(this.job == d.job)
+					return this.sec- d.sec;
+			else 
+				return this.job - d.job;
 		}
 	}
 	
     static public int solution(int[][] jobs) {
     	int answer = 0;
-    	
-    	//정렬하고 안하고의 어떤 효과?!
-    	PriorityQueue<Disk> ll = new PriorityQueue<>();
-    	List<Disk> pq = new LinkedList<>();
-    	
-    	
+
+    	PriorityQueue<Disk> pq = new PriorityQueue<>();
+    	LinkedList<Disk> list = new LinkedList<>();
+
     	for(int i=0; i<jobs.length; i++)
-    		ll.add(new Disk(jobs[i][0], jobs[i][1]));
-    	
+    		pq.add(new Disk(jobs[i][0], jobs[i][1]));
+
     	for(int i=0; i<jobs.length; i++) {
-    		pq.add(new Disk(ll.peek().sec, ll.peek().job));
-    		ll.poll();
+    		list.add(pq.poll());
     	}
     	
     	int time = 0;
     	
-    	while(!pq.isEmpty()) {
-    		//pq.get(i).sec가 time보다 작거나 같을때만 동작한다
-    		for(int i=0; i<pq.size(); i++) {
-    			if(time >= pq.get(i).sec) {
-    				time += pq.get(i).job;
-    				answer += (time - pq.get(i).sec);
-    				pq.remove(i);
-    				break;
-    			}
-    			
-        		//sec가 time보다 크면 그 차례가 올때까지 기다리기
-        		if(i == pq.size()-1)
+        while(!list.isEmpty()) {
+        	for(int i=0; i<list.size(); i++) {
+        		if(time>= list.get(i).sec) {
+        			time += list.get(i).job;
+        			answer += time- list.get(i).sec; //종료시각은 끝난시간 - 시작시간
+        			list.remove(i);
+        			break; // 한번하면 끝내기
+        		}
+        		//시작시간이 다  time보다 크다면 -> disk에 안들어온 것이니 지금 시작 불가능
+        		//끝까지 다 봤다면
+        		if(i == list.size()-1)
         			time++;
-    		}
-    	}
+        	}
+        }
     	
     	return answer/jobs.length;
-    }	
+    }
+    
+    public static void main(String[] args) {
+		int j[][] = {{0,3},{1,9},{2,6}};
+		int jj[][] = {{0,3},{1,9},{500,6}};
+		
+		solution(jj);
+	}
     
 }
