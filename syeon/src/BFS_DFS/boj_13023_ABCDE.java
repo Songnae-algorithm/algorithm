c30c0f46b3c37e5998cafa520bbf6898f8e977ac
unknown <angela7199@naver.com>
2020-07-03 18:18:36 +0900

    알고스팟미완

--- syeon/src/BFS_DFS/boj_13023_ABCDE.java
+++ syeon/src/BFS_DFS/boj_13023_ABCDE.java
@@ -1,6 +1,26 @@
 package BFS_DFS;
 
-public class boj_13023_ABCDE {
+import java.util.*;
 
-	//수정됬나요?
+public class boj_13023_ABCDE {
+	
+	public static void main(String[] args) {
+		//문제이해안됨 ㅂ2
+		Scanner sc =new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int in=sc.nextInt();
+		
+		int arr[][]=new int[a][a];
+		
+		for(int i=0;i<in;i++) {
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			arr[x][y]=1;
+			arr[y][x]=1;
+			
+		}//arr에 입력 
+		
+		
+	}
 }
